1b0ae738535fe191d0c77551dd3691abbcd77d62
Moritz Müller <moritzm.mueller@hotmail.com>
2017-06-02 11:56:08 +0200

    wtf

--- Code/RSS-Feeder/app/src/main/java/com/projektarbeit/rss_feeder/control/Parser.java
+++ Code/RSS-Feeder/app/src/main/java/com/projektarbeit/rss_feeder/control/Parser.java
@@ -1,4 +1,25 @@
 package com.projektarbeit.rss_feeder.control;
 
-public class Parser {
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+class Parser {
+    static Feed parse(String feedxml) throws IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        try {
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        Document doc = builder.parse(feedxml);
+
+
+    }
 }
