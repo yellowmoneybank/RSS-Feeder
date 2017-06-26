d8d461f25a21d3d11ff5035f1c3e2f248b393373
Moritz Müller <moritzm.mueller@hotmail.com>
2017-06-26 14:44:58 +0200

    Implemented Parser

--- Code/RSS-Feeder/app/src/main/java/com/projektarbeit/rss_feeder/control/Parser.java
+++ Code/RSS-Feeder/app/src/main/java/com/projektarbeit/rss_feeder/control/Parser.java
@@ -1,25 +1,66 @@
 package com.projektarbeit.rss_feeder.control;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 class Parser {
-    static Feed parse(String feedxml) throws IOException, SAXException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = null;
+
+    private String feedXML;
+    private ArrayList<Feed> items;
+
+    public Parser(String feedXML) {
+        this.feedXML = feedXML;
         try {
-            builder = factory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
+            items = extractItems(feedXML);
+        } catch (IOException | ParserConfigurationException | SAXException e) {
             e.printStackTrace();
         }
-        Document doc = builder.parse(feedxml);
+    }
+
+    private ArrayList<Feed> extractItems(String feedXML) throws IOException, SAXException, ParserConfigurationException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(feedXML);
+        doc.getDocumentElement().normalize();
+
+        NodeList itemList = doc.getElementsByTagName("item");
+
+        ArrayList<Feed> itemsArrayList = new ArrayList<Feed>();
+        for (int i = 0; i < itemList.getLength(); i++) {
+            Node node = itemList.item(i);
+            Feed feed = generateFeedFromNode(node);
+            itemsArrayList.add(feed);
+
+        }
 
 
+        return itemsArrayList;
+    }
+
+    private Feed generateFeedFromNode(Node node) {
+        Feed feed = null;
+        NodeList childs = node.getChildNodes();
+        for (int i = 0; i < childs.getLength(); i++) {
+            switch (childs.item(i).getNodeName()){
+
+                // TODO Hier wird der Feed erstellt mit switch
+
+            }
+        }
+        return feed;
+    }
+
+
+    public ArrayList<Feed> getItems() {
+        return items;
     }
 }
