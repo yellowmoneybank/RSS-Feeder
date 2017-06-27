e10cdea4dec4ad8a3a03062dbb5addd2eb798482
Moritz Müller <moritzm.mueller@hotmail.com>
2017-06-27 13:50:53 +0200

    Implemented refreshAllFolders and delete Folder

--- Code/RSS-Feeder/app/src/main/java/com/projektarbeit/rss_feeder/control/FeedContainer.java
+++ Code/RSS-Feeder/app/src/main/java/com/projektarbeit/rss_feeder/control/FeedContainer.java
@@ -24,9 +24,22 @@ public class FeedContainer {
     }
 
     public void refreshAllFolders() {
-
+        for (int i = 0; i < allFolders.size(); i++) {
+            new RefreshFolderThread(allFolders.get(i)).run();
+        }
     }
-    public void deleteFolder() {
+    public void deleteFolder(Folder folder) {
+        allFolders.remove(folder);
+    }
+    private class RefreshFolderThread extends Thread{
+        private Folder folder;
 
+        public RefreshFolderThread(Folder folder){
+            this.folder = folder;
+        }
+        @Override
+        public void run() {
+            folder.refreshFolder();
+        }
     }
 }
